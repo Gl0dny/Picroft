c6593c30680f53c028f42958ac370d1f074d5bce
Gl0dny <krystian.glodek1717@gmail.com>
2022-10-08 21:00:54 +0200

    control handle

--- my-robot-skill/__init__.py
+++ my-robot-skill/__init__.py
@@ -9,32 +9,31 @@ class MyRobot(Mycroftskill):
         super().__init__()
         self.base_url = self.settings.get("base_url")
 
+def handle_control(self, end_point, dialog_verb):
+    try:
+        requests.post(self.base_url + end_point)
+        #searching for dialogs which are to be outputed
+        self.speak_dialog('Robot')
+        self.speak_dialog(dialog_verb)
+    except:
+        self.speak_dialog("UnableToReach")
+        #print to mycroft console
+        LOG.exception("There was an error connecting to the robot")
+
+def create_skill():
+    return MyRobot()
+
 #searching for words
 @intent_handler(IntentBuilder("")   
                 .require("Robot")
                 .require("TestRainbow"))
 def handle_test_rainbow(self, message):
-    try:
-        requests.post(self.base_url + "/run/test_rainbow")  
-        #searching for dialogs which are to be outputed
-        self.speak_dialog('Robot')     
-        self.speak_dialog('TestingRainbow')
-    except:
-        self.speak_dialog("UnableToReach")
-        #print to mycroft console
-        LOG.exception("There was an error connecting to the robot")    
+    self.handle_control("/run/test_rainbow", "TestingRainbow")   
 
 @intent_handler(IntentBuilder("")
                 .require("Robot")
                 .require("stop"))
 def handle_stop(self, message):
-    try:
-        requests.post(self.base_url + "/stop")
-        self.speak_dialog('Robot')
-        self.speak_dialog('stopping')
-    except:
-        self.speak_dialog("UnableToReach")
-        LOG.exception("There was an error connecting to the robot")
+    self.handle_control("/stop", "stopping")
 
-def create_skill():
-    return MyRobot()
+
